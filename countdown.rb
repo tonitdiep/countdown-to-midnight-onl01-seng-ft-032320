50f80f2b072c21ae4e3b9fc7c8163e7919e598b1
Toni  Diep <adiep123@gmail.com>
2020-03-25 23:23:41 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,17 +3,17 @@
 def countdown(number)
 # x = 0
   while number > 0
-    puts "#{number} SECONDS!"
+    puts "#{number} SECOND(S)!"
     number -= 1
-  end
-  return "HAPPY NEW YEAR!"
+   end
+   return "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(num)
   while num > 0
-    puts "#{num} SECONDS!"
+    puts "#{num} SECOND(S)!"
     num -= 1
     sleep(num)
-  end
-  return "HAPPY NEW YEAR!"
+   end
+   return "HAPPY NEW YEAR!"
 end
