68aae732335f9a7a076af14eb225b7a81a576af0
Toni  Diep <adiep123@gmail.com>
2020-03-25 22:31:12 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,11 +2,10 @@
 
 def countdown_with_sleep
 # x = 0
-  while x > 10 
+  while x > 10
   until == 10
     puts "Counting down #{number} SECONDS!"
     x += 1
   return "HAPPY NEW YEAR!"
   end
 end
-  
